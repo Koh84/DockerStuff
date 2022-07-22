0bc1bfd147776a4bf13dcf62749f0d66051ec924
Kelvin <koh84@hotmail.com>
2022-07-22 10:42:17 +0100

    check and compare token in authorization header with the one in redis

--- controllers/signin.js
+++ controllers/signin.js
@@ -31,8 +31,31 @@ const handleSignin = (db, bcrypt, req, res) =>{
     .catch(err => Promise.reject('wrong credentials'))
 }
 
-const getAuthTokenId = () => {
-    console.log('auth ok');
+const getAuthTokenId = async (req, res) => {
+    const { authorization } = req.headers;
+    console.log('running getAuthTokenId :', authorization);
+
+    const value = await redisClient.get(authorization);
+    console.log(value);
+    if(value) {
+        console.log('return id in redisClient.get', value);
+        return res.json({id: value})
+    } else {
+        console.log('error in redisClient.get');
+        return res.status(400).json('Unauthorized');
+    }
+    // return redisClient.get(authorization, (err, reply) => {
+    //     console.log('redishClient.get');
+    //     console.log('authorization :', authorization);
+    //     console.log('err :', err);
+    //     console.log('reply :', reply);
+    //     if(err || !reply) {
+    //         console.log('error in redisClient.get');
+    //         return res.status(400).json('Unauthorized');
+    //     }
+    //     console.log('return id ini redisClient.get', reply);
+    //     return res.json({id: reply})
+    // })
 }
 
 const signToken = (email) => {
@@ -59,7 +82,7 @@ const createSessions = (user) => {
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
     const { authorization } = req.headers;
-    return authorization ? getAuthTokenId() : 
+    return authorization ? getAuthTokenId(req, res) : 
         handleSignin(db, bcrypt, req, res)
             .then(data => {
                 return data.id && data.email ? createSessions(data) : Promise.reject(data);
